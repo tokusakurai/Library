e6feb9f690bab662d60e808b8c1604b6823ab3d7
tokusakurai <78581123+tokusakurai@users.noreply.github.com>
2021-02-25 15:06:43 +0900

    Create Trie.cpp

---
+++ String/Trie.cpp
@@ -0,0 +1,82 @@
+
+//トライ木
+//計算量 文字列追加・検索:O(|S|)
+
+//verified with
+//https://atcoder.jp/contests/tenka1-2016-final/tasks/tenka1_2016_final_c
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<int char_size, char base>
+struct Trie{
+    struct Node{
+        vector<int> next, accept;
+        int count; //子以下に追加された文字列の数
+
+        Node() : next(char_size, -1), count(0) {}
+    };
+
+    vector<Node> nodes;
+
+    Trie() {nodes.emplace_back();}
+
+    int count() const {return nodes.front().count;}
+
+    int size() const {return nodes.size();}
+
+    void insert(const string &s, int id){ //sを追加
+        int now = 0;
+        for(int i = 0; i < (int)s.size(); i++){
+            int &next = nodes[now].next[s[i]-base];
+            if(next == -1){
+                next = size(), nodes.emplace_back();
+            }
+            nodes[now].count++, now = next;
+        }
+        nodes[now].count++, nodes[now].accept.push_back(id);
+    }
+
+    void insert(const string &s) {insert(s, count());}
+
+    bool search(const string &s, bool prefix = false) const{ //s(のprefix)を検索
+        int now = 0;
+        for(int i = 0; i < s.size(); i++){
+            now = nodes[now].next[s[i]-base];
+            if(now == -1) return false;
+        }
+        return (prefix)? true : !nodes[now].accept.empty();
+    }
+};
+
+int main(){
+    string S; int M; cin >> S >> M;
+    int N = S.size();
+
+    Trie<26, 'a'> trie;
+
+    for(int i = 0; i < M; i++){
+        string P; cin >> P;
+        trie.insert(P);
+    }
+
+    vector<int> W(M);
+    for(int i = 0; i < M; i++) cin >> W[i];
+
+    vector<int> dp(N+1, 0);
+    
+    for(int i = 0; i < N; i++){
+        int now = 0;
+        for(int j = 0; j <= 200; j++){
+            if(now == -1) break;
+            for(auto &e: trie.nodes[now].accept){
+                dp[i+j] = max(dp[i+j], dp[i]+W[e]);
+            }
+            if(i+j == N) break;
+            now = trie.nodes[now].next[S[i+j]-'a'];
+        }
+        dp[i+1] = max(dp[i+1], dp[i]);
+    }
+
+    cout << dp[N] << '\n';
+}
