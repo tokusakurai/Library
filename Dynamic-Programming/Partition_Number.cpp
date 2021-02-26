29f332475f2f60072d843f2f40c493ee4d608aa3
tokusakurai <Togo.39rai@gmail.com>
2021-02-26 16:14:45 +0900

    Update Partition_Number.cpp

--- Dynamic-Programming/Partition_Number.cpp
+++ Dynamic-Programming/Partition_Number.cpp
@@ -11,6 +11,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<typename T>
+vector<vector<T>> Partition_Number(int n, int k){ //分割数P(n,k):=整数nをちょうどk個の非負整数の和で表す方法(順序は区別しない)
+    vector<vector<T>> dp(n+1, vector<T>(k+1, 0));
+    dp[0][0] = 1;
+    for(int i = 0; i <= n; i++){
+        for(int j = 1; j <= k; j++){
+            dp[i][j] = dp[i][j-1];
+            if(i >= j) dp[i][j] += dp[i-j][j];
+        }
+    }
+    return dp;
+}
+
 const int MOD = 1000000007;
 
 template<int mod>
@@ -97,19 +110,6 @@ struct Mod_Int{
 };
 
 using mint = Mod_Int<MOD>;
-
-template<typename T>
-vector<vector<T>> Partition_Number(int n, int k){ //分割数P(n,k):=整数nをちょうどk個の非負整数の和で表す方法(順序は区別しない)
-    vector<vector<T>> dp(n+1, vector<T>(k+1, 0));
-    dp[0][0] = 1;
-    for(int i = 0; i <= n; i++){
-        for(int j = 1; j <= k; j++){
-            dp[i][j] = dp[i][j-1];
-            if(i >= j) dp[i][j] += dp[i-j][j];
-        }
-    }
-    return dp;
-}
 
 int main(){
     int N, K; cin >> N >> K;
