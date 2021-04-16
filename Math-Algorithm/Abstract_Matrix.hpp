728c09959dd7bb31f6c35560bc25a359212e4e65
tokusakurai <Togo.39rai@gmail.com>
2021-04-16 11:21:15 +0900

    Change cpp to hpp

--- Math-Algorithm/Abstract_Matrix.hpp
+++ Math-Algorithm/Abstract_Matrix.hpp
@@ -1,6 +1,6 @@
 
 //抽象化された行列
-//計算量 積：O(N*M*P)、K乗：O(N^3*log(K))
+//計算量 積 : O(N*M*P)、K乗 : O(N^3*log(K))
 
 //概要
 //半環(R,+,*)に対して行列の積演算を行える。
@@ -10,30 +10,31 @@
 //verified with
 //https://atcoder.jp/contests/abc009/tasks/abc009_4
 
+#pragma once
 #include <bits/stdc++.h>
 using namespace std;
 
-using type = unsigned int;
-const type e0 = 0, e1 = UINT_MAX; //0元、1元
-
+template<typename T, T (*add)(T, T), T(*mul)(T, T), T e0, T e1>
 struct Semi_Ring{
-    type x;
+    T x;
     Semi_Ring() {}
-    Semi_Ring(type x) : x(x){}
+    Semi_Ring(T x) : x(x){}
 
     Semi_Ring &operator += (const Semi_Ring &p) {
-        x ^= p.x;
-        return *this;
+        if(x == e0) return *this = p.x;
+        if(p.x == e0) return *this;
+        return *this = add(x, p.x);
     }
 
     Semi_Ring &operator *= (const Semi_Ring &p) {
-        x &= p.x;
-        return *this;
+        if(x == e1) return *this = p.x;
+        if(p.x == e1) return *this;
+        return *this = mul(x, p.x);
     }
-    
-    //(a+b)+c = a+(b+c)、0+a = a+0 = a、a+b = b+a
-    //(a*b)*c = a*(b*c)、1*a = a*1 = a
-    //a*(b+c) = a*b+a*c、(a+b)*c = a*c+b*c
+
+    //(a+b)+c = a+(b+c), 0+a = a+0 = a, a+b = b+a
+    //(a*b)*c = a*(b*c), 1*a = a*1 = a
+    //a*(b+c) = a*b+a*c, (a+b)*c = a*c+b*c
     //0*a = a*0 = 0
     //これらを満たす演算の組として(+,*)、(XOR,AND)、(min,+)、(max,+)などがある
 
@@ -46,18 +47,19 @@ struct Semi_Ring{
     }
 
     friend istream &operator >> (istream &is, Semi_Ring &p){
-        long long a;
+        T a;
         is >> a;
         p = Semi_Ring(a);
         return is;
     }
 };
 
-template<typename T>
-struct Abstracted_Matrix{
+template<typename t, t (*add)(t, t), t(*mul)(t, t), t e0, t e1>
+struct Abstract_Matrix{
+    using T = Semi_Ring<t, add, mul, e0, e1>;
     vector<vector<T>> A;
 
-    Abstracted_Matrix(int n, int m) : A(n, vector<T>(m, T(e0))) {}
+    Abstract_Matrix(int n, int m) : A(n, vector<T>(m, T(e0))) {}
 
     int height() const {return A.size();}
 
@@ -67,16 +69,16 @@ struct Abstracted_Matrix{
 
     inline vector<T> &operator [] (int k) {return A[k];}
 
-    static Abstracted_Matrix I(int l){
-        Abstracted_Matrix ret(l, l);
+    static Abstract_Matrix I(int l){
+        Abstract_Matrix ret(l, l);
         for(int i = 0; i < l; i++) ret[i][i] = T(e1);
         return ret;
     }
 
-    Abstracted_Matrix &operator *= (const Abstracted_Matrix &B){
+    Abstract_Matrix &operator *= (const Abstract_Matrix &B){
         int n = height(), m = width(), p = B.width();
         assert(m == B.height());
-        Abstracted_Matrix ret(n, p);
+        Abstract_Matrix ret(n, p);
         for(int i = 0; i < n; i++){
             for(int k = 0; k < m; k++){
                 for(int j = 0; j < p; j++){
@@ -88,28 +90,15 @@ struct Abstracted_Matrix{
         return *this;
     }
 
-    Abstracted_Matrix operator * (const Abstracted_Matrix &B) const {return Abstracted_Matrix(*this) *= B;}
+    Abstract_Matrix operator * (const Abstract_Matrix &B) const {return Abstract_Matrix(*this) *= B;}
 
-    Abstracted_Matrix pow(long long k){
+    Abstract_Matrix pow(long long k){
         int n = height(), m = width();
         assert(n == m);
-        Abstracted_Matrix now = *this, ret = I(n);
+        Abstract_Matrix now = *this, ret = I(n);
         for(; k > 0; k >>= 1, now *= now){
             if(k&1) ret *= now;
         }
         return ret;
     }
 };
-
-int main(){
-    int N, K; cin >> N >> K; K--;
-
-    Abstracted_Matrix<Semi_Ring> A(N, N), x(N, 1);
-
-    for(int i = 0; i < N; i++) cin >> x[N-1-i][0];
-    for(int i = 0; i < N; i++) cin >> A[0][i];
-    for(int i = 0; i < N-1; i++) A[i+1][i] = e1;
-
-    A = A.pow(K), A *= x;
-    cout << A[N-1][0] << '\n';
-}
