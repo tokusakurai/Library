334c765a78bd7d0f5c099426dd0949b43b22304e
tokusakurai <Togo.39rai@gmail.com>
2021-08-06 15:47:33 +0900

    change variable name

--- Math-Algorithm/Kitamasa.hpp
+++ Math-Algorithm/Kitamasa.hpp
@@ -16,45 +16,49 @@ using namespace std;
 
 template<typename T>
 struct Kitamasa{
-    //f(x) := x^N-c[N-1]*x^(N-1)-・・・-c[1]*x-c[0]
-    //a[k] = c[0]*a[k-N]+c[1]*a[k-N+1]+・・・+c[N-1]*a[k-1]
+    //d項間線形漸化式 a[n] = c[1]*a[n-1]+c[2]*a[n-2]+・・・+c[d]*a[n-d]
     const vector<T> a, c;
+    const int d;
+    //f(x) := x^d-c[1]*x^(d-1)-・・・-c[d-1]*x-c[d]
+    vector<T> f;
 
-    Kitamasa(const vector<T> &a, const vector<T> &c) : a(a), c(c) {}
+    Kitamasa(const vector<T> &a, const vector<T> &c) : a(a), c(c), d(a.size()){
+        f.resize(d+1);
+        f[d] = 1;
+        for(int i = 1; i <= d; i++) f[d-i] = -c[i];
+    }
 
     vector<T> mul(const vector<T> &p, const vector<T> &q) const{ //p(x)*q(x)をf(x)で割った余り
-        assert(p.size() == q.size() && p.size() >= 2);
-        int n = p.size();
-        vector<T> ret(2*n-1, 0);
+        int n = p.size(), m = q.size();
+        vector<T> ret(n+m-1, 0);
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++) ret[i+j] += p[i]*q[j];
+            for(int j = 0; j < m; j++) ret[i+j] += p[i]*q[j];
         }
-        for(int i = 2*n-2; i >= n; i--){
-            for(int j = 0; j < n; j++){
-                ret[i-n+j] += ret[i]*c[j];
+        for(int i = n+m-2; i >= d; i--){
+            for(int j = 1; j <= d; j++){
+                ret[i-j] -= ret[i]*f[d-j];
             }
         }
-        ret.resize(n);
+        ret.resize(d);
         return ret;
     }
 
-    vector<T> pow(vector<T> p, long long k) const{ //p(x)^kをf(x)で割った余り
-        int n = p.size();
-        vector<T> ret(n, 0);
+    vector<T> pow(vector<T> p, long long n) const{ //p(x)^nをf(x)で割った余り
+        vector<T> ret(1, 0);
         ret[0] = 1;
-        while(k){
-            if(k&1) ret = mul(ret, p);
-            p = mul(p, p), k >>= 1;
+        while(n){
+            if(n&1) ret = mul(ret, p);
+            p = mul(p, p), n >>= 1;
         }
         return ret;
     }
 
-    T operator [] (int k) const{ //a[k](0-indexed)
-        int n = a.size();
-        vector<T> x(n, 0);
-        x[1] = 1, x = pow(x, k);
+    T operator [] (long long n) const{ //a[n](0-indexed)
+        if(n < d) return a[n];
+        vector<T> x(d+1, 0);
+        x[1] = 1, x = pow(x, n);
         T ret = 0;
-        for(int i = 0; i < n; i++) ret += x[i]*a[i];
+        for(int i = 0; i < d; i++) ret += x[i]*a[i];
         return ret;
     }
 };
