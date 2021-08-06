845b6e63ed0a6edaeee6b5393b55638a9d6ad88c
tokusakurai <Togo.39rai@gmail.com>
2021-08-06 15:52:38 +0900

    Create Fast Kitamasa

--- Math-Algorithm/Fast_Kitamasa.hpp
+++ Math-Algorithm/Fast_Kitamasa.hpp
@@ -1,21 +1,14 @@
-
-//kitamasa法(D階の線形漸化式で与えられる数列の第N項を求める)
-//計算量 O(D^2*log(N))
-
-//概要
-//a_k = Σb_i*a_i (i = 0,1,....,D-1)の形で表したい。
-//f(x) := x^d-c[1]*x^(d-1)-・・・-c[d-1]*x-c[d]とすると、
-//x^kをf(x)で割った余りのx^iの項の係数がb_iに対応する。
-
-//verified with
-//https://atcoder.jp/contests/tdpc/tasks/tdpc_fibonacci
-
 #pragma once
 #include <bits/stdc++.h>
 using namespace std;
 
-template<typename T>
+#include "../Math-Algorithm/FPS.hpp"
+
+template<int mod, int primitive_root>
 struct Kitamasa{
+    using T = Mod_Int<mod>;
+    using FPS = Formal_Power_Series<mod, primitive_root>;
+
     //d項間線形漸化式 a[n] = c[1]*a[n-1]+c[2]*a[n-2]+・・・+c[d]*a[n-d]
     const vector<T> a, c;
     const int d;
@@ -29,22 +22,13 @@ struct Kitamasa{
     }
 
     vector<T> mul(const vector<T> &p, const vector<T> &q) const{ //p(x)*q(x)をf(x)で割った余り
-        int n = p.size(), m = q.size();
-        vector<T> ret(n+m-1, 0);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++) ret[i+j] += p[i]*q[j];
-        }
-        for(int i = n+m-2; i >= d; i--){
-            for(int j = 1; j <= d; j++){
-                ret[i-j] -= ret[i]*f[d-j];
-            }
-        }
-        ret.resize(d);
+        FPS ret = FPS(p)*FPS(q);
+        ret %= FPS(f);
         return ret;
     }
 
     vector<T> pow(vector<T> p, long long n) const{ //p(x)^nをf(x)で割った余り
-        vector<T> ret(1, 0);
+        vector<T> ret(d, 0);
         ret[0] = 1;
         while(n){
             if(n&1) ret = mul(ret, p);
@@ -54,9 +38,9 @@ struct Kitamasa{
     }
 
     T operator [] (long long n) const{ //a[n](0-indexed)
-        if(n < d) return a[n];
         vector<T> x(d+1, 0);
         x[1] = 1, x = pow(x, n);
+        x.resize(d);
         T ret = 0;
         for(int i = 0; i < d; i++) ret += x[i]*a[i];
         return ret;
