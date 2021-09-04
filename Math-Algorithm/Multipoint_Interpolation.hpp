b8cc0618eae230aae51104f1e09d581dd4c142b7
tokusakurai <Togo.39rai@gmail.com>
2021-09-04 16:34:27 +0900

    Update Lagrange Interpolation

---
+++ Math-Algorithm/Multipoint_Interpolation.hpp
@@ -0,0 +1,62 @@
+
+// ラグランジュ補間(多点)
+// 計算量 O((N+M)*log(N+M)+log(MOD))
+
+// 概要
+// (N-1)次以下の多項式f(x)についてf(0),f(1),...,f(N-1)の値が与えられたときに、与えられた1点cと自然数Mについてf(c),f(c+1),...,f(c+M-1)を求める。
+// F[i] = f(i)/Π(i-j)(j≠i), G[i] = 1/(c-N+1+i)
+// とすると、FとGを畳み込みした後のN-1+i項目がf(c+i)に相当する。
+// 0 ≦ c+i < Nのときは0除算が発生するので、場合分けする必要がある。
+
+// verified with
+// https://judge.yosupo.jp/problem/shift_of_sampling_points_of_polynomial
+
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../Math-Algorithm/NTT.hpp"
+
+template <typename T>
+vector<T> multipoint_interpolation(const vector<T> &ys, const T &c, const int &m) {
+    using NTT_ = Number_Theorem_Transform<T>;
+    int n = ys.size();
+    vector<T> ifac(n);
+    ifac[n - 1] = 1;
+    for (int i = 1; i < n; i++) ifac[n - 1] *= i;
+    ifac[n - 1] = T(1) / ifac[n - 1];
+    for (int i = n - 1; i > 0; i--) ifac[i - 1] = ifac[i] * i;
+    vector<T> f(n);
+    for (int i = 0; i < n; i++) {
+        if ((n - 1 - i) & 1)
+            f[i] = -ys[i] * ifac[i] * ifac[n - 1 - i];
+        else
+            f[i] = ys[i] * ifac[i] * ifac[n - 1 - i];
+    }
+    vector<T> p(n + m, 1), ip(n + m);
+    for (int i = 1; i < n + m; i++) {
+        if (c - n + i == 0)
+            p[i] = p[i - 1];
+        else
+            p[i] = p[i - 1] * (c - n + i);
+    }
+    ip[n + m - 1] = T(1) / p[n + m - 1];
+    for (int i = n + m - 1; i > 0; i--) {
+        if (c - n + i == 0)
+            ip[i - 1] = ip[i];
+        else
+            ip[i - 1] = ip[i] * (c - n + i);
+    }
+    vector<T> g(n + m - 1);
+    for (int i = 0; i < n + m - 1; i++) g[i] = p[i] * ip[i + 1];
+    f = NTT_::convolve(f, g);
+    vector<T> ret(m);
+    for (int i = 0; i < m; i++) {
+        int x = (c + i).x;
+        if (0 <= x && x < n)
+            ret[i] = ys[x];
+        else
+            ret[i] = f[n - 1 + i] * p[n + i] * ip[i];
+    }
+    return ret;
+}
--- Math-Algorithm/Single_Point_Interpolation.hpp
+++ Math-Algorithm/Single_Point_Interpolation.hpp
@@ -1,11 +1,11 @@
 
 // ラグランジュ補間(1点)
-// 計算量 O(N+log(M))
+// 計算量 O(N+log(MOD))
 
 // 概要
-// N次多項式f(x)についてf(0),f(1),...,f(N)の値が与えられたときに、与えられた1点xについてf(x)を求める。
+// (N-1)次以下の多項式f(x)についてf(0),f(1),...,f(N-1)の値が与えられたときに、与えられた1点cについてf(c)を求める。
 // ラグランジュ補間をすると、
-// f(x) = Σy_i*Π{(x-j)/(i-j)}(j≠i)
+// f(c) = Σf(i)*Π{(c-j)/(i-j)}(j≠i)
 // であるから、階乗の逆数と(x-i)の累積積を左右から求めることで高速化する。
 
 // verified with
@@ -16,7 +16,7 @@
 using namespace std;
 
 template <typename T>
-T Lagrange_interpolation(const vector<T> &ys, T x) {
+T single_point_interpolation(const vector<T> &ys, T c) {
     int n = ys.size();
     vector<T> ifac(n);
     ifac[n - 1] = 1;
@@ -24,9 +24,14 @@ T Lagrange_interpolation(const vector<T> &ys, T x) {
     ifac[n - 1] = T(1) / ifac[n - 1];
     for (int i = n - 1; i > 0; i--) ifac[i - 1] = ifac[i] * i;
     vector<T> L(n + 1, 1), R(n + 1, 1);
-    for (int i = 0; i < n; i++) L[i + 1] = L[i] * (x - i);
-    for (int i = n - 1; i >= 0; i--) R[i] = R[i + 1] * (x - i);
+    for (int i = 0; i < n; i++) L[i + 1] = L[i] * (c - i);
+    for (int i = n - 1; i >= 0; i--) R[i] = R[i + 1] * (c - i);
     T ret = 0;
-    for (int i = 0; i < n; i++) ret += ys[i] * L[i] * R[i + 1] * ifac[i] * ifac[n - 1 - i] * ((n - 1 - i) & 1 ? -1 : 1);
+    for (int i = 0; i < n; i++) {
+        if ((n - 1 - i) & 1)
+            ret -= ys[i] * L[i] * R[i + 1] * ifac[i] * ifac[n - 1 - i];
+        else
+            ret += ys[i] * L[i] * R[i + 1] * ifac[i] * ifac[n - 1 - i];
+    }
     return ret;
 }
