fb66be6e3762596c2d2cfcdf990b1b03c77a10d1
tokusakurai <Togo.39rai@gmail.com>
2021-09-29 20:18:24 +0900

    Update Template

--- Other/Template.hpp
+++ Other/Template.hpp
@@ -17,17 +17,57 @@ using pii = pair<int, int>;
 using pil = pair<int, ll>;
 using pli = pair<ll, int>;
 using pll = pair<ll, ll>;
-const int MOD = 1000000007;
-// const int MOD = 998244353;
-const int inf = (1 << 30) - 1;
-const ll INF = (1LL << 60) - 1;
+
 template <typename T>
 bool chmax(T &x, const T &y) {
     return (x < y) ? (x = y, true) : false;
 }
+
 template <typename T>
 bool chmin(T &x, const T &y) {
     return (x > y) ? (x = y, true) : false;
+}
+
+template <typename T>
+int flg(T x, int i) {
+    return (x >> i) & 1;
+}
+
+template <typename T>
+void print(const vector<T> &v, T x = 0) {
+    int n = v.size();
+    for (int i = 0; i < n; i++) cout << v[i] + x << (i == n - 1 ? '\n' : ' ');
+}
+
+template <typename T>
+void printn(const vector<T> &v, T x = 0) {
+    int n = v.size();
+    for (int i = 0; i < n; i++) cout << v[i] + x << '\n';
+}
+
+template <typename T>
+int lb(const vector<T> &v, T x) {
+    return lower_bound(begin(v), end(v), x) - begin(v);
+}
+
+template <typename T>
+int ub(const vector<T> &v, T x) {
+    return upper_bound(begin(v), end(v), x) - begin(v);
+}
+
+template <typename T>
+void rearrange(vector<T> &v) {
+    sort(begin(v), end(v));
+    v.erase(unique(begin(v), end(v)), end(v));
+}
+
+template <typename T>
+vector<int> id_sort(const vector<T> &v, bool greater = false) {
+    int n = v.size();
+    vector<int> ret(n);
+    iota(begin(ret), end(ret), 0);
+    sort(begin(ret), end(ret), [&](int i, int j) { return greater ? v[i] > v[j] : v[i] < v[j]; });
+    return ret;
 }
 
 struct io_setup {
@@ -37,3 +77,8 @@ struct io_setup {
         cout << fixed << setprecision(15);
     }
 } io_setup;
+
+const int inf = (1 << 30) - 1;
+const ll INF = (1LL << 60) - 1;
+const int MOD = 1000000007;
+// const int MOD = 998244353;
