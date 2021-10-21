1802d489a0a3b2cf6d3dc0c1422d7bfac89b73fb
tokusakurai <Togo.39rai@gmail.com>
2021-10-21 11:54:04 +0900

    Fixed explanation

--- Dynamic-Programming/Mo_Example.hpp
+++ Dynamic-Programming/Mo_Example.hpp
@@ -1,9 +1,9 @@
 
-// Moのアルゴリズム(例)
-// 計算量 O((N+Q)*√N)
+// Moのアルゴリズム (例)
+// 計算量 O((n+q)√n)
 
 // 概要
-// 区間の伸縮をO(1)で行えるため、計算量はO((N+Q)*√N)
+// 区間の伸縮をO(1)で行えるため、計算量はO((n+q)√n)
 
 // verified with
 // https://atcoder.jp/contests/abc174/tasks/abc174_f
