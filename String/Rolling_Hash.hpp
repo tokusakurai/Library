85dd2b2077d4b939edb3367ccfe82cf38d834c42
tokusakurai <Togo.39rai@gmail.com>
2022-10-27 17:28:55 +0900

    create rolling hash

--- String/Rolling_Hash.hpp
+++ String/Rolling_Hash.hpp
@@ -1,5 +1,5 @@
 
-// ローリングハッシュ、配列ハッシュ (mod：(2^61)-1、基数：mod の原始根からランダムに取る)
+// ローリングハッシュ、固定長ハッシュ (mod：(2^61)-1、基数：mod の原始根からランダムに取る)
 // 計算量 構築：O(n)、ハッシュクエリ：O(1)
 
 // 概要
@@ -17,124 +17,92 @@ using namespace std;
 #include "../Other/Random.hpp"
 
 using ull = unsigned long long;
+const ull mod = (1ULL << 61) - 1;
 
+ull hash_mod(ull x) {
+    ull ret = (x >> 61) + (x & mod);
+    return ret - (ret >= mod ? mod : 0);
+}
+
+ull hash_mul(ull x, ull y) {
+    x = hash_mod(x), y = hash_mod(y);
+    ull x1 = x >> 31, x2 = x & ((1ULL << 31) - 1), y1 = y >> 31, y2 = y & ((1ULL << 31) - 1);
+    ull z = x1 * y2 + x2 * y1, z1 = z >> 30, z2 = z & ((1ULL << 30) - 1);
+    return hash_mod(x1 * y1 * 2 + x2 * y2 + z1 + (z2 << 31));
+}
+
+ull hash_pow(ull x, ull n) {
+    ull ret = 1;
+    for (; n > 0; n >>= 1, x = hash_mul(x, x)) {
+        if (n & 1) ret = hash_mul(ret, x);
+    }
+    return ret;
+}
+
+// m 以上の base を乱択で生成する
+ull generate_base(ull m = (1ULL << 50)) {
+    while (true) {
+        ull k = rng(mod);
+        if (gcd(mod - 1, k) != 1) continue;
+        ull base = hash_pow(3, k);
+        if (base >= m) return base;
+    }
+    return 0;
+}
+
+// 0 が含まれないように
+template <typename T = string>
 struct Rolling_Hash {
-    const ull mod = (1ULL << 61) - 1;
-
-    ull calc_mod(ull x) const {
-        ull ret = (x >> 61) + (x & mod);
-        return ret - (ret >= mod ? mod : 0);
-    }
-
-    ull mul(ull x, ull y) const {
-        x = calc_mod(x), y = calc_mod(y);
-        ull x1 = x >> 31, x2 = x & ((1ULL << 31) - 1), y1 = y >> 31, y2 = y & ((1ULL << 31) - 1);
-        ull z = x1 * y2 + x2 * y1, z1 = z >> 30, z2 = z & ((1ULL << 30) - 1);
-        return calc_mod(x1 * y1 * 2 + x2 * y2 + z1 + (z2 << 31));
-    }
-
-    ull pow(ull x, ull n) const {
-        ull ret = 1;
-        for (; n > 0; n >>= 1, x = mul(x, x)) {
-            if (n & 1) ret = mul(ret, x);
-        }
-        return ret;
-    }
-
-    ull base; // 基数
+    const int n;
+    const ull base; // 基数
     vector<ull> hashed, pw;
 
-    // 基数は乱数を用いて生成する
-    void set_base() {
-        while (true) {
-            ull k = rng(mod);
-            if (gcd(mod - 1, k) != 1) continue;
-            base = pow(3, k);
-            if (base < 256) continue;
-            break;
-        }
-    }
-
-    Rolling_Hash(const string &s) {
-        set_base();
-        int n = s.size();
-        hashed.resize(n + 1), pw.resize(n + 1);
-        hashed[0] = 0, pw[0] = 1;
+    Rolling_Hash(const T &s, ull base) : n(s.size()), base(base) {
+        hashed.assign(n + 1, 0), pw.assign(n + 1, 1);
         for (int i = 0; i < n; i++) {
-            pw[i + 1] = mul(pw[i], base);
-            hashed[i + 1] = mul(hashed[i], base) + s[i];
+            pw[i + 1] = hash_mul(pw[i], base);
+            hashed[i + 1] = hash_mul(hashed[i], base) + s[i];
             if (hashed[i + 1] >= mod) hashed[i + 1] -= mod;
         }
     }
 
     // 文字列の [l,r) の部分のハッシュ値
-    ull query(int l, int r) const {
-        ull ret = hashed[r] + mod - mul(hashed[l], pw[r - l]);
+    ull get_hash(int l, int r) const {
+        ull ret = hashed[r] + mod - hash_mul(hashed[l], pw[r - l]);
         return ret - (ret >= mod ? mod : 0);
     }
 
-    ull get_hash(const string &s) const {
-        ull ret = 0;
-        for (int i = 0; i < (int)s.size(); i++) {
-            ret = mul(ret, base) + s[i];
-            if (ret >= mod) ret -= mod;
-        }
-        return ret;
-    }
+    ull get_all_hash() const { return hashed[n]; }
 };
 
 template <typename T>
-struct Array_Hash {
-    const ull mod = (1ULL << 61) - 1;
-
-    ull calc_mod(ull x) const {
-        ull ret = (x >> 61) + (x & mod);
-        return ret - (ret >= mod ? mod : 0);
-    }
-
-    ull mul(ull x, ull y) const {
-        x = calc_mod(x), y = calc_mod(y);
-        ull x1 = x >> 31, x2 = x & ((1ULL << 31) - 1), y1 = y >> 31, y2 = y & ((1ULL << 31) - 1);
-        ull z = x1 * y2 + x2 * y1, z1 = z >> 30, z2 = z & ((1ULL << 30) - 1);
-        return calc_mod(x1 * y1 * 2 + x2 * y2 + z1 + (z2 << 31));
-    }
-
-    ull pow(ull x, ull n) const {
-        ull ret = 1;
-        for (; n > 0; n >>= 1, x = mul(x, x)) {
-            if (n & 1) ret = mul(ret, x);
-        }
-        return ret;
-    }
-
-    ull base;
+struct Fixed_Size_Hash {
+    const int n;
+    const ull base;
+    vector<T> v;
+    ull hashed;
     vector<ull> pw;
 
-    void set_base(T m) {
-        Random_Number_Generator rng;
-        while (true) {
-            ull k = rng(mod);
-            if (gcd(mod - 1, k) != 1) continue;
-            base = pow(3, k);
-            if (base <= ull(m)) continue;
-            break;
+    Fixed_Size_Hash(const vector<T> &v, ull base) : n(v.size()), base(base), v(v) {
+        hashed = 0;
+        pw.assign(n + 1, 1);
+        for (int i = 0; i < n; i++) {
+            pw[i + 1] = hash_mul(pw[i], base);
+            hashed = hash_mul(hashed, base) + v[i];
+            if (hashed >= mod) hashed -= mod;
         }
     }
 
-    // 配列の長さ、要素の最大値
-    Array_Hash(int n, T m) {
-        set_base(m);
-        pw.resize(n + 1);
-        pw[0] = 1;
-        for (int i = 0; i < n; i++) { pw[i + 1] = mul(pw[i], base); }
+    Fixed_Size_Hash(int m, const T &x, ull base) : Fixed_Size_Hash(vector<T>(m, x), base) {}
+
+    ull add(int i, const T &x) {
+        hashed += hash_mul(pw[n - 1 - i], mod + x);
+        if (hashed >= mod) hashed -= mod;
+        v[i] += x;
+        return hashed;
     }
 
-    ull get_hash(const vector<T> &v) const {
-        ull ret = 0;
-        for (int i = 0; i < (int)v.size(); i++) {
-            ret += mul(v[i], pw[i]);
-            if (ret >= mod) ret -= mod;
-        }
-        return ret;
-    }
+    ull change(int i, const T &x) { return add(i, x - v[i]); }
+
+    ull get_hash() const { return hashed; }
 };
