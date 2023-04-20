ba47f391c52f2c3568911730166d82c79f64e571
tokusakurai <Togo.39rai@gmail.com>
2023-04-20 16:10:56 +0900

    create get subtree function

--- Tree/Rerooting.hpp
+++ Tree/Rerooting.hpp
@@ -12,34 +12,37 @@
 // https://atcoder.jp/contests/s8pc-4/tasks/s8pc_4_d
 // https://atcoder.jp/contests/dp/tasks/dp_v
 // https://atcoder.jp/contests/abc220/tasks/abc220_f
+// https://atcoder.jp/contests/abc298/tasks/abc298_h
 
 #pragma once
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename sum_t, typename key_t, bool directed = false>
+template <typename sum_t, typename key_t>
 struct Rerooting {
     struct edge {
         int to;
         key_t data;
-        sum_t dp, ndp; // to 側の部分木 dp (辺も含む)、from 側の部分木 dp (辺は含まない)
-        edge(int to, key_t data, sum_t dp, sum_t ndp) : to(to), data(data), dp(dp), ndp(ndp) {}
+        sum_t dp, ndp; // to 側の部分木 dp（辺も含む）、from 側の部分木 dp（辺は含まない）
+        edge(int to, const key_t &data, const sum_t &dp, const sum_t &ndp) : to(to), data(data), dp(dp), ndp(ndp) {}
     };
 
     using F = function<sum_t(sum_t, sum_t)>;
     using G = function<sum_t(sum_t, key_t)>;
     vector<vector<edge>> es;
-    vector<sum_t> subdp, dp;  // 部分木の dp、全方位の dp
+    const int n;
+    vector<sum_t> subdp, dp; // 部分木の dp、全方位の dp
+    unordered_map<long long, sum_t> mp;
     const F f;                // 1 頂点を間に挟んで隣り合う 2 つの部分木の情報をマージ
     const G g;                // 部分木の根に 1 本辺を足す
     const sum_t e1;           // f の単位元
     const vector<sum_t> base; // 1 頂点の場合の dp の値
 
-    Rerooting(int n, const F &f, const G &g, const sum_t &e1, const vector<sum_t> &base) : es(n), subdp(base), dp(n), f(f), g(g), e1(e1), base(base) {}
+    Rerooting(int n, const F &f, const G &g, const sum_t &e1, const vector<sum_t> &base) : es(n), n(n), subdp(base), dp(n), f(f), g(g), e1(e1), base(base) {}
 
     void add_edge(int from, int to, const key_t &data) {
         es[from].emplace_back(to, data, e1, base[from]);
-        if (!directed) es[to].emplace_back(from, data, e1, base[to]);
+        es[to].emplace_back(from, data, e1, base[to]);
     }
 
     void dfs_sub(int now, int pre = -1) {
@@ -63,7 +66,11 @@ struct Rerooting {
         for (int i = (int)es[now].size() - 1; i >= 0; i--) {
             auto &e = es[now][i];
             e.ndp = f(e.ndp, S);
-            if (e.to != pre) dfs_all(e.to, e.ndp, now);
+            if (e.to != pre) {
+                mp[1LL * n * now + e.to] = e.ndp;
+                mp[1LL * n * e.to + now] = subdp[e.to];
+                dfs_all(e.to, e.ndp, now);
+            }
             S = f(S, e.dp);
         }
     }
@@ -73,4 +80,7 @@ struct Rerooting {
         dfs_all(0, e1);
         return dp;
     }
+
+    // 辺 {u,v} で切った後の u 側の部分木 dp
+    sum_t get_subtree(int u, int v) { return mp[1LL * n * u + v]; }
 };
