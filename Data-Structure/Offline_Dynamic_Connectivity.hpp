6ce5a35a97322407470a4ed5a47bfd1161993a1b
tokusakurai <Togo.39rai@gmail.com>
2023-09-19 12:58:34 +0900

    create offline dynamic connectivity

---
+++ Data-Structure/Offline_Dynamic_Connectivity.hpp
@@ -0,0 +1,109 @@
+
+// オフライン dynamic connectivity (クエリがオフラインで与えられるとき、辺の削除にも対応できる union find)
+// 計算量 O(n + q log(q)log(n))
+
+// 概要
+// クエリの順に辺を並べると、ある辺が挿入されてから削除されるまでの間は区間になる。
+// 時刻の区間をセグ木の様に管理すると、ある辺が存在する区間をセグ木上の O(log(q)) 個の区間に分解できる。
+// セグ木上を dfs しながら undo union find を使うことによってクエリに答えることができる。
+
+// verified with
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2235
+
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../Data-Structure/Undo_Union_Find_Tree.hpp"
+
+struct Offline_Dynamic_Connectivity {
+    struct edge {
+        int u, v;
+        edge(int u, int v) : u(u), v(v) {}
+    };
+
+    int n, q;
+    vector<tuple<int, int, int, int>> eve;
+    vector<tuple<int, int, int, int>> qs;
+
+    Offline_Dynamic_Connectivity(int n) : n(n), q(0) {}
+
+    // 時刻 t に辺 {u,v} を追加
+    void insert(int t, int u, int v) {
+        if (u > v) swap(u, v);
+        eve.emplace_back(t, 1, u, v);
+    }
+
+    // 時刻 t に辺 {u,v} を削除
+    void erase(int t, int u, int v) {
+        if (u > v) swap(u, v);
+        eve.emplace_back(t, -1, u, v);
+    }
+
+    // 時刻 t 以前の辺の追加削除が終了した後に辺 u,v が連結かどうかを判定
+    void query(int t, int u, int v) { qs.emplace_back(t, q++, u, v); }
+
+    vector<bool> solve() {
+        sort(begin(eve), end(eve));
+        vector<int> ts;
+        for (auto [t, c, u, v] : eve) ts.push_back(t);
+        ts.erase(unique(begin(ts), end(ts)), end(ts));
+
+        int m = ts.size();
+        ts.push_back(INT_MAX);
+        int si = 1;
+        while (si < m) si <<= 1;
+        vector<vector<pair<int, int>>> seg(2 * si);
+
+        auto add_edge = [&](int u, int v, int l, int r) {
+            l += si, r += si;
+            while (l < r) {
+                if (l & 1) seg[l++].emplace_back(u, v);
+                if (r & 1) seg[--r].emplace_back(u, v);
+                l >>= 1, r >>= 1;
+            }
+        };
+
+        map<pair<int, int>, vector<int>> mp;
+        for (auto [t, c, u, v] : eve) {
+            t = lower_bound(begin(ts), end(ts), t) - begin(ts);
+            if (c == 1) {
+                mp[pair(u, v)].push_back(t);
+            } else {
+                auto it = mp.find(pair(u, v));
+                if (it != end(mp)) {
+                    int s = it->second.back();
+                    it->second.pop_back();
+                    add_edge(u, v, s, t);
+                }
+            }
+        }
+        for (auto [e, v] : mp) {
+            for (auto s : v) add_edge(e.first, e.second, s, m);
+        }
+
+        Undo_Union_Find_Tree uf(n);
+        vector<bool> ret(q, false);
+        sort(begin(qs), end(qs));
+        int ptr = 0;
+        while (ptr < q && get<0>(qs[ptr]) < ts[0]) ptr++;
+
+        auto dfs = [&](auto &&dfs, int i) -> void {
+            for (auto [u, v] : seg[i]) uf.unite(u, v);
+            if (si <= i && i < si + m) {
+                while (ptr < q && get<0>(qs[ptr]) < ts[i - si + 1]) {
+                    auto [t, id, u, v] = qs[ptr++];
+                    ret[id] = uf.same(u, v);
+                }
+            }
+            if (i < si) {
+                dfs(dfs, 2 * i);
+                dfs(dfs, 2 * i + 1);
+            }
+            uf.undo((int)seg[i].size());
+        };
+
+        dfs(dfs, 1);
+        return ret;
+    }
+};
--- Data-Structure/README.md
+++ Data-Structure/README.md
@@ -8,11 +8,13 @@
 
 - [二部 Union-Find Tree](Bipartite_Union_Find_Tree.hpp) (各頂点を二色で塗りわけ、2 頂点を同色にする、異色にするのクエリを扱える)
 
-- [重み付き Union-Find Tree](Weighted_Union_Find_Tree.hpp)（各頂点に重みを与えたとき、weight(y) - weight(x) = w の制約を扱える）
+- [重み付き Union-Find Tree](Weighted_Union_Find_Tree.hpp) (各頂点に重みを与えたとき、weight(y) - weight(x) = w の制約を扱える)
 
-- [部分永続 Union-Find Tree](Partitially_Persistent_Union_Find_Tree.hpp)（各 Union 操作に時刻を与えたとき、任意の時刻での Find クエリを扱える）
+- [部分永続 Union-Find Tree](Partitially_Persistent_Union_Find_Tree.hpp) (各 Union 操作に時刻を与えたとき、任意の時刻での Find クエリを扱える)
 
-- [Undo 可能 Union-Find Tree](Undo_Union_Find_Tree.hpp)（Union 操作を巻き戻すことができる）
+- [Undo 可能 Union-Find Tree](Undo_Union_Find_Tree.hpp) (Union 操作を巻き戻すことができる)
+
+- [オフライン dynamic connectivity](Offline_Dynamic_Connectivity.hpp) (オフラインの場合、辺の削除を扱える)
 
 ## Binary Indexed Tree
 
@@ -34,13 +36,13 @@ Binary Indexed Tree に Binary Indexed Tree を乗せることで、2 次元平�
 
 - [Segment Tree](Segment_Tree.hpp)
 
-遅延評価（必要になるまで更新を保留すること）を用いることによって、区間更新を扱うこともできる。
+遅延評価 (必要になるまで更新を保留すること)を用いることによって、区間更新を扱うこともできる。
 
-- [双対 Segment Tree](Dual_Segment_Tree.hpp)（作用素の二項演算がモノイドの性質を満たすとき、区間更新と 1 点取得を扱える）
+- [双対 Segment Tree](Dual_Segment_Tree.hpp) (作用素の二項演算がモノイドの性質を満たすとき、区間更新と 1 点取得を扱える)
 
-- [遅延評価付き Segment Tree](Lazy_Segment_Tree.hpp)（上記 2 つを組み合わせることで区間更新と区間演算を扱える）
+- [遅延評価付き Segment Tree](Lazy_Segment_Tree.hpp) (上記 2 つを組み合わせることで区間更新と区間演算を扱える)
 
-- [Segment Tree Beats](Segment_Tree_Beats.hpp)（区間 chmin・chmax を扱える）
+- [Segment Tree Beats](Segment_Tree_Beats.hpp) (区間 chmin・chmax を扱える)
 
 Segment Tree に Segment Tree や双対 Segment Tree を乗せることで、2 次元平面上の 1 点更新と矩形演算や矩形更新と 1 点取得を扱うこともできる。
 
@@ -62,17 +64,17 @@ Segment Tree に Segment Tree や双対 Segment Tree を乗せることで、2 �
 
 配列を適当なサイズごとのバケットに分割して区間クエリを処理するテクニック。Segment Tree と比べて計算量は落ちるが、扱えるクエリの幅が大きい。
 
-- [平方分割（雛形）](Sqrt_Decomposition_Template.hpp)
+- [平方分割 (雛形)](Sqrt_Decomposition_Template.hpp)
 
-- [平方分割（例）](Sqrt_Decomposition_Example.hpp)（区間 chmin、区間 chmax、区間 add、区間 sum クエリを扱える）
+- [平方分割 (例)](Sqrt_Decomposition_Example.hpp) (区間 chmin、区間 chmax、区間 add、区間 sum クエリを扱える)
 
 ## 平衡二分探索木
 
-- [スプレー木](Splay_Tree.hpp)（スプレー操作を行うことで平衡を保つ二分探索木）
+- [スプレー木](Splay_Tree.hpp) (スプレー操作を行うことで平衡を保つ二分探索木)
 
 ## Binary Trie
 
-- [Binary Trie](Binary_Trie.hpp)（非負整数値を 2 進数表記することで Trie 木を構築する）
+- [Binary Trie](Binary_Trie.hpp) (非負整数値を 2 進数表記することで Trie 木を構築する)
 
 ## ウェーブレット行列
 
