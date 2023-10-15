c089becbbed00229653bb1fc1775a5e8c9704c3a
tokusakurai <Togo.39rai@gmail.com>
2023-10-15 16:22:06 +0900

    update F2 matrix

--- Math-Algorithm/F2_Matrix.hpp
+++ Math-Algorithm/F2_Matrix.hpp
@@ -13,10 +13,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 template <typename T>
 struct F2_Matrix {
     vector<T> A;
     int n, m;
+
+    F2_Matrix() = default;
 
     F2_Matrix(int n, int m) : A(n), n(n), m(m) {}
 
@@ -51,39 +54,43 @@ struct F2_Matrix {
         return row_reduction(b);
     }
 
-    // Ax = b の解の 1 つと解空間の基底の組を返す
-    vector<vector<int>> Gaussian_elimination(vector<int> b) {
-        row_reduction(b);
-        vector<vector<int>> ret;
-        vector<int> p(n, m);
-        vector<bool> is_zero(m, true);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (A[i][j] == 1) {
-                    p[i] = j;
-                    break;
-                }
-            }
-            if (p[i] < m) {
-                is_zero[p[i]] = false;
-            } else if (b[i] == 1) {
-                return {};
+    int rank() const { return Matrix(*this).row_reduction(); }
+};
+
+// Ax = b の解の 1 つと解空間の基底の組を返す
+template <typename T>
+vector<vector<int>> F2_linear_equation(F2_Matrix<T> A, vector<int> b) {
+    int n = A.n, m = A.m;
+    A.row_reduction(b);
+    vector<vector<int>> ret;
+    vector<int> p(n, m);
+    vector<bool> is_zero(m, true);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (A[i][j] == 1) {
+                p[i] = j;
+                break;
             }
         }
-        vector<int> x(m, 0);
+        if (p[i] < m) {
+            is_zero[p[i]] = false;
+        } else if (b[i] == 1) {
+            return {};
+        }
+    }
+    vector<int> x(m, 0);
+    for (int i = 0; i < n; i++) {
+        if (p[i] < m) x[p[i]] = b[i];
+    }
+    ret.push_back(x);
+    for (int j = 0; j < m; j++) {
+        if (!is_zero[j]) continue;
+        x[j] = 1;
         for (int i = 0; i < n; i++) {
-            if (p[i] < m) x[p[i]] = b[i];
+            if (p[i] < n) x[p[i]] ^= A[i][j];
         }
         ret.push_back(x);
-        for (int j = 0; j < m; j++) {
-            if (!is_zero[j]) continue;
-            x[j] = 1;
-            for (int i = 0; i < n; i++) {
-                if (p[i] < n) x[p[i]] ^= A[i][j];
-            }
-            ret.push_back(x);
-            x[j] = 0;
-        }
-        return ret;
+        x[j] = 0;
     }
-};
+    return ret;
+}
