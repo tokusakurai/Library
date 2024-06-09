fb459a6b4159c90ebc51da141119a1139f8f64b2
tokusakurai <Togo.39rai@gmail.com>
2024-06-09 13:43:32 +0900

    [update] scc

--- Graph/Strongly-Connected_Components.hpp
+++ Graph/Strongly-Connected_Components.hpp
@@ -15,31 +15,27 @@ using namespace std;
 
 #include "../Graph/Graph_Template.hpp"
 
+template <typename G>
 struct Strongly_Connected_Components {
-    struct edge {
-        int to, id;
-        edge(int to, int id) : to(to), id(id) {}
-    };
-
-    vector<vector<edge>> es;
     vector<int> st;
     vector<int> ord, low, comp;
-    const int n;
-    int m;
+    int components_number;
 
-    Strongly_Connected_Components(int n) : es(n), ord(n), low(n), comp(n), n(n), m(0) { st.reserve(n); }
-
-    void add_edge(int from, int to) {
-        es[from].emplace_back(to, m);
-        m++;
+    Strongly_Connected_Components(const G &g) : ord(g.n, -1), low(g.n), comp(g.n) {
+        int cnt_ord = 0, cnt_group = 0;
+        for (int i = 0; i < g.n; i++) {
+            if (ord[i] == -1) _dfs(i, cnt_ord, cnt_group, g);
+        }
+        for (int i = 0; i < g.n; i++) comp[i] = cnt_group - 1 - comp[i];
+        components_number = cnt_group;
     }
 
-    void _dfs(int now, int &cnt_ord, int &cnt_group) {
+    void _dfs(int now, int &cnt_ord, int &cnt_group, const G &g) {
         ord[now] = low[now] = cnt_ord++;
         st.push_back(now);
-        for (auto &e : es[now]) {
+        for (auto &e : g[now]) {
             if (ord[e.to] == -1) {
-                _dfs(e.to, cnt_ord, cnt_group);
+                _dfs(e.to, cnt_ord, cnt_group, g);
                 low[now] = min(low[now], low[e.to]);
             } else {
                 low[now] = min(low[now], ord[e.to]);
@@ -49,7 +45,7 @@ struct Strongly_Connected_Components {
             while (true) {
                 int v = st.back();
                 st.pop_back();
-                ord[v] = n;
+                ord[v] = g.n;
                 comp[v] = cnt_group;
                 if (v == now) break;
             }
@@ -57,25 +53,15 @@ struct Strongly_Connected_Components {
         }
     }
 
-    int decompose() {
-        fill(begin(ord), end(ord), -1);
-        int cnt_ord = 0, cnt_group = 0;
-        for (int i = 0; i < n; i++) {
-            if (ord[i] == -1) _dfs(i, cnt_ord, cnt_group);
-        }
-        for (int i = 0; i < n; i++) comp[i] = cnt_group - 1 - comp[i];
-        return cnt_group;
-    }
-
-    Graph<true> make_graph() {
-        Graph<true> G(decompose());
-        for (int i = 0; i < n; i++) {
-            for (auto &e : es[i]) {
+    Graph<true> make_graph(const G &g) {
+        Graph<true> g2(components_number);
+        for (int i = 0; i < g.n; i++) {
+            for (auto &e : g[i]) {
                 int u = comp[i], v = comp[e.to];
-                if (u != v) G.add_edge(u, v);
+                if (u != v) g2.add_edge(u, v);
             }
         }
-        return G;
+        return g2;
     }
 
     int operator[](int k) const { return comp[k]; }
