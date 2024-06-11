f5ade41c13b47d1fa1f6bc6502915e3218a2d7ac
tokusakurai <Togo.39rai@gmail.com>
2024-06-11 20:32:58 +0900

    [add] circum circle

--- Geometry/All.hpp
+++ Geometry/All.hpp
@@ -251,6 +251,16 @@ Circle Apollonius(const Point &p, const Point &q, const Real &a, const Real &b) 
     return c;
 }
 
+// p,q,r の外接円 (3 点が同一直線上にある場合は false)
+pair<bool, Circle> circum_circle(Point p, Point q, Point r) {
+    if (distance(p, q) <= EPS || distance(q, r) <= EPS) return make_pair(false, Circle(0, 0));
+    Line l1 = vertical_bisector(p, q);
+    Line l2 = vertical_bisector(q, r);
+    vector<Point> ps = crosspoint(l1, l2);
+    if (ps.empty()) return make_pair(false, Circle(0, 0));
+    return make_pair(true, Circle(ps[0], distance(ps[0], p)));
+}
+
 // http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_3_A&lang=ja
 Real area(const vector<Point> &p) {
     Real ret = 0.0;
--- Geometry/Apollonius.hpp
+++ Geometry/Apollonius.hpp
@@ -10,6 +10,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#include "../Geometry/Crosspoint.hpp"
+#include "../Geometry/Distance.hpp"
+#include "../Geometry/Intersect.hpp"
 #include "../Geometry/Structure.hpp"
 
 Line vertical_bisector(const Point &p, const Point &q) {
@@ -25,4 +28,14 @@ Circle Apollonius(const Point &p, const Point &q, const Real &a, const Real &b) 
     c.p = (p1 + p2) * 0.5;
     c.r = abs(p1 - p2) * 0.5;
     return c;
+}
+
+// p,q,r の外接円 (3 点が同一直線上にある場合は false)
+pair<bool, Circle> circum_circle(Point p, Point q, Point r) {
+    if (distance(p, q) <= EPS || distance(q, r) <= EPS) return make_pair(false, Circle(0, 0));
+    Line l1 = vertical_bisector(p, q);
+    Line l2 = vertical_bisector(q, r);
+    vector<Point> ps = crosspoint(l1, l2);
+    if (ps.empty()) return make_pair(false, Circle(0, 0));
+    return make_pair(true, Circle(ps[0], distance(ps[0], p)));
 }
