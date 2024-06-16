e378017fd3d14e950bac279b6ce784f23bd1dac7
tokusakurai <Togo.39rai@gmail.com>
2024-06-16 10:34:34 +0900

    [add] Hash Int structre

--- String/Rolling_Hash.hpp
+++ String/Rolling_Hash.hpp
@@ -16,63 +16,142 @@ using namespace std;
 
 #include "../Other/Random.hpp"
 
-using ull = unsigned long long;
-const ull mod = (1ULL << 61) - 1;
+struct Hash_Int {
+    using ull = unsigned long long;
+    static const ull mod = (1ULL << 61) - 1;
+    ull x;
 
-ull hash_mod(ull x) {
-    ull ret = (x >> 61) + (x & mod);
-    return ret - (ret >= mod ? mod : 0);
-}
+    Hash_Int() : x(0) {}
 
-ull hash_mul(ull x, ull y) {
-    x = hash_mod(x), y = hash_mod(y);
-    ull x1 = x >> 31, x2 = x & ((1ULL << 31) - 1), y1 = y >> 31, y2 = y & ((1ULL << 31) - 1);
-    ull z = x1 * y2 + x2 * y1, z1 = z >> 30, z2 = z & ((1ULL << 30) - 1);
-    return hash_mod(x1 * y1 * 2 + x2 * y2 + z1 + (z2 << 31));
-}
+    Hash_Int(ull y) : x(y) { normalize(); }
 
-ull hash_pow(ull x, ull n) {
-    ull ret = 1;
-    for (; n > 0; n >>= 1, x = hash_mul(x, x)) {
-        if (n & 1) ret = hash_mul(ret, x);
+    static ull get_mod() { return mod; }
+
+    void normalize() {
+        ull y = (x >> 61) + (x & mod);
+        if (y >= mod) y -= mod;
+        x = y;
     }
-    return ret;
-}
 
-// m 以上の base を乱択で生成する
-ull generate_base(ull m = (1ULL << 50)) {
-    while (true) {
-        ull k = rng(mod);
-        if (gcd(mod - 1, k) != 1) continue;
-        ull base = hash_pow(3, k);
-        if (base >= m) return base;
+    // m 以上の base を乱択で生成する
+    static Hash_Int generate_base(Hash_Int m = (1ULL << 50)) {
+        while (true) {
+            ull k = rng(mod);
+            if (gcd(mod - 1, k) != 1) continue;
+            Hash_Int base = Hash_Int(3).pow(k);
+            if (base >= m) return base;
+        }
+        return 0;
     }
-    return 0;
-}
+
+    Hash_Int &operator+=(const Hash_Int &p) {
+        x += p.x;
+        if (x >= mod) x -= mod;
+        return *this;
+    }
+
+    Hash_Int &operator-=(const Hash_Int &p) {
+        x += mod - p.x;
+        if (x >= mod) x -= mod;
+        return *this;
+    }
+
+    Hash_Int &operator*=(const Hash_Int &p) {
+        ull y = p.x;
+        ull x1 = x >> 31, x2 = x & ((1ULL << 31) - 1);
+        ull y1 = y >> 31, y2 = y & ((1ULL << 31) - 1);
+        ull z = x1 * y2 + x2 * y1;
+        ull z1 = z >> 30, z2 = z & ((1ULL << 30) - 1);
+        x = x1 * y1 * 2 + x2 * y2 + z1 + (z2 << 31);
+        normalize();
+        return *this;
+    }
+
+    Hash_Int &operator/=(const Hash_Int &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+
+    Hash_Int &operator++() { return *this += Hash_Int(1); }
+
+    Hash_Int operator++(int) {
+        Hash_Int tmp = *this;
+        ++*this;
+        return tmp;
+    }
+
+    Hash_Int &operator--() { return *this -= Hash_Int(1); }
+
+    Hash_Int operator--(int) {
+        Hash_Int tmp = *this;
+        --*this;
+        return tmp;
+    }
+
+    Hash_Int operator-() const { return Hash_Int(mod - x); }
+
+    Hash_Int operator+(const Hash_Int &p) const { return Hash_Int(*this) += p; }
+
+    Hash_Int operator-(const Hash_Int &p) const { return Hash_Int(*this) -= p; }
+
+    Hash_Int operator*(const Hash_Int &p) const { return Hash_Int(*this) *= p; }
+
+    Hash_Int operator/(const Hash_Int &p) const { return Hash_Int(*this) /= p; }
+
+    bool operator==(const Hash_Int &p) const { return x == p.x; }
+
+    bool operator!=(const Hash_Int &p) const { return x != p.x; }
+
+    bool operator<(const Hash_Int &p) const { return x < p.x; }
+
+    bool operator>(const Hash_Int &p) const { return x > p.x; }
+
+    bool operator<=(const Hash_Int &p) const { return x <= p.x; }
+
+    bool operator>=(const Hash_Int &p) const { return x >= p.x; }
+
+    Hash_Int inverse() const {
+        assert(*this != Hash_Int(0));
+        return pow(mod - 2);
+    }
+
+    Hash_Int pow(long long k) const {
+        Hash_Int now = *this, ret = 1;
+        for (; k > 0; k >>= 1, now *= now) {
+            if (k & 1) ret *= now;
+        }
+        return ret;
+    }
+
+    friend ostream &operator<<(ostream &os, const Hash_Int &p) { return os << p.x; }
+
+    friend istream &operator>>(istream &is, Hash_Int &p) {
+        ull a;
+        is >> a;
+        p = Hash_Int(a);
+        return is;
+    }
+};
 
 // 0 が含まれないように
 template <typename T = string>
 struct Rolling_Hash {
     const int n;
-    const ull base; // 基数
-    vector<ull> hashed, pw;
+    const Hash_Int base; // 基数
+    vector<Hash_Int> hashed, pw;
 
-    Rolling_Hash(const T &s, ull base) : n(s.size()), base(base) {
+    Rolling_Hash(const T &s, Hash_Int base) : n(s.size()), base(base) {
         hashed.assign(n + 1, 0), pw.assign(n + 1, 1);
         for (int i = 0; i < n; i++) {
-            pw[i + 1] = hash_mul(pw[i], base);
-            hashed[i + 1] = hash_mul(hashed[i], base) + s[i];
-            if (hashed[i + 1] >= mod) hashed[i + 1] -= mod;
+            pw[i + 1] = pw[i] * base;
+            hashed[i + 1] = hashed[i] * base + s[i];
         }
     }
 
     // 文字列の [l,r) の部分のハッシュ値
-    ull get_hash(int l, int r) const {
-        ull ret = hashed[r] + mod - hash_mul(hashed[l], pw[r - l]);
-        return ret - (ret >= mod ? mod : 0);
-    }
+    Hash_Int get_hash(int l, int r) const { return hashed[r] - hashed[l] * pw[r - l]; }
 
-    ull get_all_hash() const { return hashed[n]; }
+    Hash_Int get_all_hash() const { return hashed[n]; }
 
     // s[l1:r1] と s[l2:r2] の最長共通接頭辞
     int lcp(int l1, int r1, int l2, int r2) {
@@ -97,31 +176,29 @@ struct Rolling_Hash {
 template <typename T>
 struct Fixed_Size_Hash {
     const int n;
-    const ull base;
+    const Hash_Int base;
     vector<T> v;
-    ull hashed;
-    vector<ull> pw;
+    Hash_Int hashed;
+    vector<Hash_Int> pw;
 
-    Fixed_Size_Hash(const vector<T> &v, ull base) : n(v.size()), base(base), v(v) {
+    Fixed_Size_Hash(const vector<T> &v, Hash_Int base) : n(v.size()), base(base), v(v) {
         hashed = 0;
         pw.assign(n + 1, 1);
         for (int i = 0; i < n; i++) {
-            pw[i + 1] = hash_mul(pw[i], base);
-            hashed = hash_mul(hashed, base) + v[i];
-            if (hashed >= mod) hashed -= mod;
+            pw[i + 1] = pw[i] * base;
+            hashed = hashed * base + v[i];
         }
     }
 
-    Fixed_Size_Hash(int m, const T &x, ull base) : Fixed_Size_Hash(vector<T>(m, x), base) {}
+    Fixed_Size_Hash(int m, const T &x, Hash_Int base) : Fixed_Size_Hash(vector<T>(m, x), base) {}
 
-    ull add(int i, const T &x) {
-        hashed += hash_mul(pw[n - 1 - i], mod + x);
-        if (hashed >= mod) hashed -= mod;
+    Hash_Int add(int i, const T &x) {
+        hashed += pw[n - 1 - i] * (Hash_Int::get_mod() + x);
         v[i] += x;
         return hashed;
     }
 
-    ull change(int i, const T &x) { return add(i, x - v[i]); }
+    Hash_Int change(int i, const T &x) { return add(i, x - v[i]); }
 
-    ull get_hash() const { return hashed; }
+    Hash_Int get_hash() const { return hashed; }
 };
